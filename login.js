9efd98bbec967a37e438d4bcf96997970c04f6d3
gguggurggu <sentmeroses121@gmail.com>
2025-02-03 10:29:50 +0900

    Changed the order

--- login.js
+++ login.js
@@ -5,6 +5,17 @@
         alert("3");
       }
 
+       // ID 란에 입력된 값을 팝업창에 띄우기
+       function popId() {
+        let userId = document.getElementById("txt-id").value;
+
+        if (!userId) {
+          alert("아이디를 입력해주세요.");
+        } else {
+          alert(userId);
+        }
+      }
+
       // var vs. let vs. const
       function compareVariable() {
         let num1 = 10;
@@ -13,15 +24,4 @@
         num1 = 20;
         alert("num1 : " + num1);
         alert("num2 : " + num2);
-      }
-
-      // ID 란에 입력된 값을 팝업창에 띄우기
-      function popId() {
-        let userId = document.getElementById("txt-id").value;
-
-        if (!userId) {
-          alert("아이디를 입력해주세요.");
-        } else {
-          alert(userId);
-        }
       }
